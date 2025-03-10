3ee479fdf454856aa3ba7588639555dda814cce5
Simone Mine <simonminekoza@gmail.com>
2025-03-10 16:22:09 +0100

    orbit controls setup

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
@@ -8,43 +10,65 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(4, 5, 11);
-camera.lookAt(0,0, 0);
+camera.position.set(4, 5, 10);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
 groundGeometry.rotateX(-Math.PI / 2);
 const groundMaterial = new THREE.MeshStandardMaterial({
-  color: 0x555555,
+  color: 0x000000,
   side: THREE.DoubleSide
 });
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+// groundMesh.castShadow = false;
+// groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
 const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
 spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
 scene.add(spotLight);
 
-const loader = new GLTFLoader()
-loader.load(
-  'headstone.gltf',
-  (gltf) => {
-    console.log('Model loaded successfully:', gltf);
-    const mesh = gltf.scene;
-    mesh.position.set(0, 1.05, -1);
-    scene.add(mesh);
-  },
-  undefined,
-  (error) => {
-    console.error('Error loading model:', error);
+const loader = new GLTFLoader();
+loader.load( 'public/headstone.gltf', function ( gltf ) {
+
+  gltf.scene.traverse((child) => {
+  if (child.isMesh) {
+    child.castShadow = true;
+    child.receiveShadow = true;
   }
-);
+})
+
+	scene.add( gltf.scene );
+  gltf.scene.position.y += 0.5;
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 
 function animate() {
   requestAnimationFrame(animate);
+  controls.update();
   renderer.render(scene, camera);
 }
 
